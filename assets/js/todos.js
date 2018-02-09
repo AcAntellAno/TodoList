181e07e1b5868a99e1b974b0952812b2f1187945
AcAntellAno <acantellano1996@gmail.com>
2018-02-08 23:20:39 -0800

    Added JS logic

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,1 +1,20 @@
 //console.log("connection successful");
+//check off specific todos by clicking
+$("li").click(function() {
+	//if line is gray
+	console.log($(this).css("color"));
+	if ($(this).css("color") === "rgb(128, 128, 128)") {
+		console.log("it is gray");
+		//turn it black
+		$(this).css({
+			color: "black",
+			textDecoration: "none"
+		});
+	} else {
+		//turn it gray
+		$(this).css({
+			color: "gray",
+			textDecoration: "line-through"
+		});
+	}
+});
