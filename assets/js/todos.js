0a38444b5b05bd188cdc6c4d0931aec592f0936e
AcAntellAno <acantellano1996@gmail.com>
2018-02-08 23:54:34 -0800

    Added fade animation for when todo is completed

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -3,3 +3,12 @@
 $("li").click(function() {
 	$(this).toggleClass("completed");
 });
+
+//click on X to delete todo
+$("span").click(function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove(); //-> this refers to li
+	}); //this is the span that we clicked on, parent gives li
+	event.stopPropagation(); //prevents bubbling up and wont trigger anything else
+});
+
