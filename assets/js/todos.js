e06ff932d6ca4065b3bb7051b607c5bd8072dd08
AcAntellAno <acantellano1996@gmail.com>
2018-02-09 00:18:17 -0800

    Added ability to add todos

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,14 +1,22 @@
 //console.log("connection successful");
 //check off specific todos by clicking
-$("li").click(function() {
+$("ul").on("click", "li", function() {
 	$(this).toggleClass("completed");
 });
 
 //click on X to delete todo
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove(); //-> this refers to li
 	}); //this is the span that we clicked on, parent gives li
 	event.stopPropagation(); //prevents bubbling up and wont trigger anything else
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){ //13 is enter key
+		var todoText = $(this).val();
+		$(this).val(""); //clears input
+		$("ul").append("<li><span>X</span> " + todoText + "</li>");
+	}
 });
 
