2117af8370cc891c76683c8501166adc2dfb7c8b
AcAntellAno <acantellano1996@gmail.com>
2018-02-09 22:33:18 -0800

    Finished styling application

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -3,20 +3,17 @@
 $("ul").on("click", "li", function() {
 	$(this).toggleClass("completed");
 });
-
 //click on X to delete todo
-$("ul").on("click", "span", function(event){
-	$(this).parent().fadeOut(500, function(){
+$("ul").on("click", "span", function(event) {
+	$(this).parent().fadeOut(500, function() {
 		$(this).remove(); //-> this refers to li
 	}); //this is the span that we clicked on, parent gives li
 	event.stopPropagation(); //prevents bubbling up and wont trigger anything else
 });
-
-$("input[type='text']").keypress(function(event){
-	if(event.which === 13){ //13 is enter key
+$("input[type='text']").keypress(function(event) {
+	if (event.which === 13) { //13 is enter key
 		var todoText = $(this).val();
 		$(this).val(""); //clears input
-		$("ul").append("<li><span>X</span> " + todoText + "</li>");
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
 	}
 });
-
